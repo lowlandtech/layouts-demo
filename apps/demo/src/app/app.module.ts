24de4757cfc1481cb70c135e52d30b449adc598f
Wendell Muntslag van Amzink <wmuntslag@live.nl>
2020-05-25 13:42:45 +0200

    chore: add admin page

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeModule } from './pages/home/home.module';
+import { AdminModule } from './pages/admin/admin.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HomeModule],
+  imports: [BrowserModule, HomeModule, AdminModule],
   providers: [],
   bootstrap: [AppComponent],
 })
